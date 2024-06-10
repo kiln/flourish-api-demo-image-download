95f8b6199e765a979774521710fd5ce64e47c92c
larsvers <lars@canva.com>
2024-06-10 12:02:35 +0100

    Minor refines and add license/README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Image download example
+
+Simple example showcasing the Live API image download functionality. Read more about it [here](https://developers.flourish.studio/api/create-visualisation/#how-to-generate-an-image-of-your-visualisation).
--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const opts = {
-    container: "#visualization",
-    api_key: "tnCbAlSozHJR7dOpI1GIcU-LUmwBOQFZZ0AD3epZ9GOeIkdE8D2_ILHB5Z3Qmbav",
-    base_visualisation_id: "17078430"
+  container: "#visualization",
+  api_key: "tnCbAlSozHJR7dOpI1GIcU-LUmwBOQFZZ0AD3epZ9GOeIkdE8D2_ILHB5Z3Qmbav",
+  base_visualisation_id: "17078430",
 };
 
-var visualization_api = new Flourish.Live(opts);
+const visualization_api = new Flourish.Live(opts);
 
-var snapshot_options = {
-    download: true,
-    format: "png", // Formats available include png, jpg & svg
-    filename: "flourish-map",
-    scale: 2,
-}
+const snapshot_options = {
+  download: true,
+  format: "png", // Formats available include png, jpg & svg
+  filename: "flourish-map",
+  scale: 2,
+};
 
-document.querySelector("button").addEventListener("click", function() {
-    visualization_api.snapshot(snapshot_options, function (error, data) {
-        if (error) {
-            console.error(error);
-            return;
-        }
-    });
-})
+document.querySelector("button").addEventListener("click", function () {
+  visualization_api.snapshot(snapshot_options, function (error, data) {
+    console.log(data);
+    if (error) {
+      console.error(error);
+      return;
+    }
+  });
+});
